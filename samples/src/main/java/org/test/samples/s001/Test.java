754e5f2c5abb81aa22d4ad21b0f8b019904ff7ac
usanin-si <SUsanin@alfabank.ru>
2023-09-19 23:05:11 +0300

    init

---
+++ samples/src/main/java/org/test/samples/s001/Test.java
@@ -0,0 +1,10 @@
+package org.test.samples.s001;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Test {
+    public static void main(String[] args) {
+        log.info("Hello");
+    }
+}
